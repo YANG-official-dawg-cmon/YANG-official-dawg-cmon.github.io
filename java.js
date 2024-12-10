95f54fac6f938eaa39bd8dc8530fe96610185665
YANG-official-dawg-cmon <yangofficialyy@gmail.com>
2024-12-09 19:19:08 -0800

    Update java.js

--- java.js
+++ java.js
@@ -1,18 +1,6 @@
 function clearForm() { 
-let firstName = document.getElementById('first-name')
-let lookinFor = document.getElementById('lookin-for')
  var x = document.getElementById("thanks");
- if (firstName.value !== "" && lookinFor.value !== "") {
+ if (container.value !== "") {
   x.style.display = "block";
-  setTimeout(clearForm2, 5)
-  } else {
-  x.style.display = "none";
   }
-}
-
-function clearForm2(){
-  let firstName = document.getElementById('first-name')
-  let lookinFor = document.getElementById('lookin-for')
-  firstName.value = ""
-  lookinFor.value = ""
 }
